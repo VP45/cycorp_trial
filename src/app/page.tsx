a11d904bd58bfa69b25c57a4b901d16d234bfe56
VP45 <pariawalaveer@gmail.com>
2023-10-17 22:46:42 +0530

    collage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 import Navbar from "@/components/Navbar";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="relative flex flex-col h-screen overflow-hidden">
-      <div className="flex justify-center h-full">
-        <Navbar />
-        <video
-          autoPlay
-          loop
-          muted
-          className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
-        >
-          <source src="/hero.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <div className="hidden md:block">
+        <div className="relative flex flex-col h-screen overflow-hidden">
+          <div className="flex justify-center h-full">
+            <Navbar />
+            <video
+              autoPlay
+              loop
+              muted
+              className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+            >
+              <source src="/hero.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+      </div>
+      <div className="md:hidden">
+      <div className="relative flex flex-col h-screen overflow-hidden">
+          <div className="flex justify-center h-full">
+            <Navbar />
+            <Image src='/bg_coll.png' alt ="collage" className="absolute z-10 w-auto min-w-full" width={1000} height={1000}/>
+          </div>
+        </div>
       </div>
     </main>
   );
